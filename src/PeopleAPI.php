c1f72c8a405a1d47103a43fff5c577deb25a0844
J. Aaron Eaton <aaron@channeleaton.com>
2014-04-24 11:46:12 -0500

    First pass at a ALP wrapper class

---
+++ src/PeopleAPI.php
@@ -0,0 +1,157 @@
+<?php
+namespace AgriLife\Core;
+
+/**
+ * Class PeopleAPI
+ * @package AgriLife\Core
+ */
+class PeopleAPI {
+
+	/**
+	 * @var SoapClient
+	 */
+	private $client;
+
+	/**
+	 * @var string
+	 */
+	private $hash;
+
+	/**
+	 * @param \SoapClient $client
+	 */
+	public function __construct( \SoapClient $client ) {
+
+		$this->set_client( $client );
+
+	}
+
+	/**
+	 * Sets the hash
+	 *
+	 * @param string $hash
+	 *
+	 * @throws \Exception If the hash isn't a Base64-encoded string
+	 */
+	public function set_hash( $hash ) {
+
+		if ( base64_encode( base64_decode( $hash ) ) != $hash ) {
+			throw new \Exception( 'The hash provided was not encoded properly' );
+		} else {
+			$this->hash = $hash;
+		}
+
+	}
+
+	/**
+	 * Returns the hash
+	 *
+	 * @return string
+	 */
+	public function get_hash() {
+
+		return $this->hash;
+
+	}
+
+	/**
+	 * Sets the SoapClient object
+	 *
+	 * @param $client
+	 */
+	protected function set_client( $client ) {
+
+		$this->client = $client;
+
+	}
+
+	/**
+	 * Returns the SoapClient
+	 *
+	 * @return SoapClient
+	 */
+	public function get_client() {
+
+		return $this->client;
+
+	}
+
+	/**
+	 * Retrieves the units from the web service and sends them to the parser
+	 * @param string $hash
+	 * @param int $entity_id
+	 *
+	 * @return array|bool Array of unit objects of successful, false if unsuccessful
+	 */
+	public function get_units( $hash, $entity_id ) {
+
+		$this->set_hash( $hash );
+
+		$all_units = $this->client->getUnits( 3, $this->hash, null, $entity_id, null, null, null, null );
+
+		if ( '200' != $all_units['ResultCode'] ) {
+			// @todo Have a log message here
+			return false;
+		}
+
+		$payload = $all_units['ResultQuery']->enc_value->data;
+
+		$parsed = $this->parse_units( $payload );
+
+		return $parsed;
+
+	}
+
+	/**
+	 * Parses the terribly structured array of units from AgriLife People
+	 *
+	 * @param array $raw_units
+	 *
+	 * @return array Array of unit objects
+	 */
+	protected function parse_units( $raw_units ) {
+
+		$parsed_units = array();
+
+		foreach ( $raw_units as $u ) {
+			$unit = new \stdClass();
+			$unit->id = $u[0];
+			$unit->number = $u[1];
+			$unit->name = $u[2];
+			$unit->parentunitid = $u[3];
+			$unit->districtid = $u[4];
+			$unit->regionid = $u[5];
+			$unit->districtname = $u[6];
+			$unit->regionname = $u[7];
+			$unit->url = $u[8];
+			$unit->phone = $u[9];
+			$unit->fax = $u[10];
+			$unit->email = $u[11];
+			$unit->address1 = $u[12];
+			$unit->address2 = $u[13];
+			$unit->city = $u[14];
+			$unit->state = $u[15];
+			$unit->zip = $u[16];
+			$unit->mailingaddress1 = $u[17];
+			$unit->mailingaddress2 = $u[18];
+			$unit->mailstop = $u[19];
+			$unit->mailingcity = $u[20];
+			$unit->mailingstate = $u[21];
+			$unit->mailingzip = $u[22];
+
+			// Some units don't have county data
+			$county_data = $u[23]->data;
+			if ( array_key_exists( 0, $county_data ) ) {
+				$unit->county = $county_data[0][2];
+			} else {
+				$unit->county = null;
+			}
+
+			array_push( $parsed_units, $unit );
+		}
+
+		return $parsed_units;
+
+	}
+
+}
---
+++ tests/test-PeopleAPI.php
@@ -0,0 +1,215 @@
+<?php
+
+class Test_PeopleAPI extends WP_UnitTestCase {
+
+	private $client;
+	private $successfulResponse;
+	private $failedResponse;
+
+	private $getUnitsHash = 'MR1ac6DLFjce4q9zl6M9Kw==';
+	private $badGetUnitsHash = 'this is a bad hash';
+
+	public function setUp() {
+
+		parent::setUp();
+
+		$enc_value = new stdClass();
+		$enc_value->columnList = array(
+			'unitid',
+			'unitnumber',
+			'unitname',
+			'parentunitid',
+			'districtid',
+			'regionid',
+			'districtname',
+			'regionname',
+			'uniturl',
+			'unitphonenumber',
+			'unitfaxnumber',
+			'unitemailaddress',
+			'address1',
+			'address2',
+			'city',
+			'state',
+			'zipcode',
+			'mailing_address1',
+			'mailing_address2',
+			'mailing_mailstop',
+			'mailing_city',
+			'mailing_state',
+			'mailing_zipcode',
+			'qCounties',
+		);
+
+		$first_county = new stdClass();
+		$first_county->columnList = array(
+			'isprimarycounty',
+			'countyid',
+			'countyname',
+		);
+		$first_county->data = array(
+			0 => array(
+				1,
+				21,
+				'Brazos',
+			),
+		);
+
+		$second_county = new stdClass();
+		$second_county->columnList = array(
+			'isprimarycounty',
+			'countyid',
+			'countyname',
+		);
+		$second_county->data = array(
+			0 => array(
+				1,
+				1,
+				'Anderson',
+			),
+		);
+
+		$enc_value->data = array(
+			0 => array(
+				310,
+				890,
+				'4-H and Youth Development',
+				'',
+				13,
+				5,
+				'Campus',
+				'Campus/Military',
+				'http://texas4-h.tamu.edu',
+				'979.845.1211',
+				'979.845.6495',
+				'texas4h@ag.tamu.edu',
+				'4180 State Highway 6',
+				'',
+				'College Station',
+				'TX',
+				'77845',
+				'Texas A&M University',
+				'2473 TAMU',
+				2473,
+				'College Station',
+				'TX',
+				'77843-2473',
+				$first_county,
+			),
+			1 => array(
+				1,
+				1,
+				'Anderson County Office',
+				258,
+				5,
+				2,
+				'East',
+				'East Region',
+				'http://anderson.agrilife.org',
+				'903.723.3735',
+				'903.723.2810',
+				'anderson-tx@tamu.edu',
+				'101 E. Oak St',
+				'',
+				'Palestine',
+				'TX',
+				'75801-2820',
+				'','','','','','',
+				$second_county,
+			),
+		);
+
+		$result = new stdClass();
+		$result->enc_type = 0;
+		$result->enc_value = $enc_value;
+
+		$this->successfulResponse = array(
+			'ResultCode' => '200',
+			'ResultMessages' => 'Success',
+			'ResultQuery' => $result,
+		);
+
+		$this->failedResponse = array(
+			'ResultCode' => '310',
+			'ResultMessages' => 'Bad data',
+			'ResultQuery' => '',
+		);
+
+
+	}
+
+	public function test_GoodHashIsAccepted() {
+
+		$client = $this->getMock( 'SoapClient', null, array( 'https://agrilifepeople.tamu.edu/api/v4.cfc?wsdl' ) );
+		$api = new AgriLife\Core\PeopleAPI( $client );
+		$api->set_hash( $this->getUnitsHash );
+
+		$this->assertEquals( $this->getUnitsHash, $api->get_hash() );
+
+	}
+
+	public function test_BadHashThrowsException() {
+
+		$this->setExpectedException( 'Exception', 'The hash provided was not encoded properly' );
+
+		$client = $this->getMock( 'SoapClient', null, array( 'https://agrilifepeople.tamu.edu/api/v4.cfc?wsdl' ) );
+		$api = new AgriLife\Core\PeopleAPI( $client );
+		$api->set_hash( $this->badGetUnitsHash );
+
+	}
+
+	public function test_GoodResponseParses() {
+
+		$client = $this->getMockFromWsdl( dirname(__FILE__) . '/ALP.wsdl' );
+		$client->expects( $this->any() )
+			->method('getUnits')
+			->will( $this->returnValue( $this->successfulResponse ) );
+
+		$api = new AgriLife\Core\PeopleAPI( $client );
+		$units = $api->get_units( $this->getUnitsHash, '2' );
+
+		foreach( $units as $key => $data ) {
+			$data = $this->successfulResponse['ResultQuery']->enc_value->data[$key];
+			$this->assertAttributeEquals( $data[0], 'id', $units[$key] );
+			$this->assertAttributeEquals( $data[1], 'number', $units[$key] );
+			$this->assertAttributeEquals( $data[2], 'name', $units[$key] );
+			$this->assertAttributeEquals( $data[3], 'parentunitid', $units[$key] );
+			$this->assertAttributeEquals( $data[4], 'districtid', $units[$key] );
+			$this->assertAttributeEquals( $data[5], 'regionid', $units[$key] );
+			$this->assertAttributeEquals( $data[6], 'districtname', $units[$key] );
+			$this->assertAttributeEquals( $data[7], 'regionname', $units[$key] );
+			$this->assertAttributeEquals( $data[8], 'url', $units[$key] );
+			$this->assertAttributeEquals( $data[9], 'phone', $units[$key] );
+			$this->assertAttributeEquals( $data[10], 'fax', $units[$key] );
+			$this->assertAttributeEquals( $data[11], 'email', $units[$key] );
+			$this->assertAttributeEquals( $data[12], 'address1', $units[$key] );
+			$this->assertAttributeEquals( $data[13], 'address2', $units[$key] );
+			$this->assertAttributeEquals( $data[14], 'city', $units[$key] );
+			$this->assertAttributeEquals( $data[15], 'state', $units[$key] );
+			$this->assertAttributeEquals( $data[16], 'zip', $units[$key] );
+			$this->assertAttributeEquals( $data[17], 'mailingaddress1', $units[$key] );
+			$this->assertAttributeEquals( $data[18], 'mailingaddress2', $units[$key] );
+			$this->assertAttributeEquals( $data[19], 'mailstop', $units[$key] );
+			$this->assertAttributeEquals( $data[20], 'mailingcity', $units[$key] );
+			$this->assertAttributeEquals( $data[21], 'mailingstate', $units[$key] );
+			$this->assertAttributeEquals( $data[22], 'mailingzip', $units[$key] );
+			$this->assertAttributeEquals( $data[23]->data[0][2], 'county', $units[$key] );
+		}
+
+	}
+
+	public function test_FailedResponseReturnsFalse() {
+
+		$client = $this->getMockFromWsdl( dirname( __FILE__ ) . '/ALP.wsdl' );
+		$client->expects( $this->any() )
+			->method( 'getUnits' )
+			->will( $this->returnValue( $this->failedResponse ) );
+
+		$api = new AgriLife\Core\PeopleAPI( $client );
+		$units = $api->get_units( $this->getUnitsHash, '2' );
+
+		$this->assertFalse( $units );
+
+	}
+
+}
