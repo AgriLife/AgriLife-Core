645054450798cdc6a0eab2c273b66738359d7b84
Zachary Watkins <watkinza@gmail.com>
2017-05-11 12:06:14 -0500

    Make flexiblecolumns_buttons.js more readable and use min-width and min-height instead of width and height so that button sizes are easier to override by stylesheets

--- js/flexiblecolumns_buttons.js
+++ js/flexiblecolumns_buttons.js
@@ -1,33 +1,45 @@
-/* Ensure headings in a row have identical heights */
+/* Ensure buttons in a row have identical heights */
 (function(){
+
   var scripts = document.getElementsByTagName('script'),
       thisscript = scripts[scripts.length-1],
-      buttons = thisscript.parentNode.querySelectorAll('a'),
+      buttons = thisscript.parentNode.querySelectorAll('a.fc-button'),
       maxWidth = 0,
       maxHeight = 0;
 
+  // Get the tallest button's height and the widest button's width
   for(var i = 0; i < buttons.length; i++){
-    var rect = buttons[i].getBoundingClientRect();
-    var width = rect.width;
-    var height = rect.height;
-    if(width > maxWidth){
+
+    var rect = buttons[i].getBoundingClientRect(),
+        width = rect.width,
+        height = rect.height;
+
+    if(width > maxWidth)
       maxWidth = Math.ceil(width);
-    }
-    if(height > maxHeight){
+
+    if(height > maxHeight)
       maxHeight = Math.ceil(height);
-    }
+
   }
+
+  // Set all buttons to those dimensions
   for(var i = 0; i < buttons.length; i++){
-    var button = buttons[i];
-    button.style.width = maxWidth + 'px';
-    var rect = button.getBoundingClientRect();
+
+    var button = buttons[i],
+        rect = button.getBoundingClientRect();
+
+    button.style.minWidth = maxWidth + 'px';
+
     // Adjust height and line height to match when buttons are scaled down
     if(rect.height < maxHeight){
-      var style = window.getComputedStyle(button);
-      var paddingTopAndBottom = parseFloat(style.getPropertyValue('padding-top')) + parseFloat(style.getPropertyValue('padding-bottom'));
-      var borderTopAndBottom = parseFloat(style.getPropertyValue('border-top-width')) + parseFloat(style.getPropertyValue('border-bottom-width'));
+      var style = window.getComputedStyle(button),
+          paddingTopAndBottom = parseFloat(style.getPropertyValue('padding-top')) + parseFloat(style.getPropertyValue('padding-bottom')),
+          borderTopAndBottom = parseFloat(style.getPropertyValue('border-top-width')) + parseFloat(style.getPropertyValue('border-bottom-width'));
       button.style.lineHeight = maxHeight - borderTopAndBottom - paddingTopAndBottom + 'px';
     }
-    button.style.height = maxHeight + 'px';
+
+    button.style.minHeight = maxHeight + 'px';
+
   }
+
 })();
