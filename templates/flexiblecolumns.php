c24385f4c14fa18b20b0a9c04c35859da1122bc3
Zachary Watkins <watkinza@gmail.com>
2017-05-11 12:15:33 -0500

    Add a stylesheet file for the flexible columns template, add the lightbox javascript file to the template page, add some class names to improve styling capability; also implement new custom fields for section titles, a second Type of Columns content with full TinyMCE editors for two columns, and a publications row type used by the AgriLife Extension Research plugin

--- templates/flexiblecolumns.php
+++ templates/flexiblecolumns.php
@@ -8,13 +8,40 @@ if ( !get_field( 'show_page_title' ) ){
   remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 }
 
+// Add assets
+add_action( 'wp_enqueue_scripts', 'fc_register_styles' );
+add_action( 'wp_enqueue_scripts', 'fc_enqueue_styles' );
 add_action( 'wp_enqueue_scripts', 'fc_register_js' );
 add_action( 'wp_footer', 'fc_enqueue_js' );
 
+function fc_register_styles() {
+
+  wp_register_style(
+    'flexible-column-styles',
+    AG_CORE_DIR_URL . 'css/flexiblecolumns.css',
+    array(),
+    '',
+    'screen'
+  );
+
+}
+
+function fc_enqueue_styles(){
+
+  wp_enqueue_style( 'flexible-column-styles' );
+
+}
+
 function fc_register_js() {
 
-  wp_register_script( 'fc_template_script',
-    AG_CORE_DIR_URL . '/js/flexiblecolumns.js',
+  wp_register_script( 'fc_template_accordion_script',
+    AG_CORE_DIR_URL . 'js/flexiblecolumns_accordion.js',
+    false,
+    true
+  );
+
+  wp_register_script( 'fc_template_lightbox_script',
+    AG_CORE_DIR_URL . 'js/flexiblecolumns_lightbox.js',
     false,
     true
   );
@@ -23,7 +50,8 @@ function fc_register_js() {
 
 function fc_enqueue_js() {
 
-  wp_enqueue_script( 'fc_template_script' );
+  wp_enqueue_script( 'fc_template_accordion_script' );
+  wp_enqueue_script( 'fc_template_lightbox_script' );
 
 }
 
@@ -40,17 +68,27 @@ function fc_repeating_content()
     while( have_rows( 'rows' ) ): the_row();
 
       $rowname = get_row()['acf_fc_layout'];
-      $rowclass = $rowname != 'buttons' ? 'row' : 'row collapse';
+      $rowclass = 'flexiblecolumns row';
+      if( $rowname == 'buttons' )
+        $rowclass .= ' collapse';
+
+      // Create the row wrapper
       $content = sprintf('<div class="%s"', $rowclass);
 
+      // Additional row HTML attributes
       if( $rowname == 'columns' ){
+
         $content .= sprintf( ' style="text-align: %s;"', get_sub_field('text_alignment') );
+
       } else if( $rowname == 'buttons' ){
+
         $content .= sprintf( ' style="text-align: %s; font-size: %srem;"', get_sub_field('text_alignment'), get_sub_field('font_size') );
+
       }
 
       $content .= '>';
 
+      // Output content based on row type
       if( $rowname == 'contact_info' ){
 
         $content .= sprintf( '<div class="small-12 medium-12 large-12 columns"><p class="vcard"><span class="fn">%s</span><br><a href="mailto:%s" class="email">%s</a><br><span class="tel">%s</span></p></div>', get_sub_field('name'), get_sub_field('email'), get_sub_field('email'), get_sub_field('phone') );
@@ -62,7 +100,7 @@ function fc_repeating_content()
         $cols = (12 - 12 % $count) / $count;
 
         foreach( $subfield as $button ){
-          $content .= sprintf( '<div class="small-12 medium-%s large-%s columns"><a class="button" style="%s" href="%s">%s</a></div>', $cols, $cols, 'max-width:100%;', $button['link'], $button['text'] );
+          $content .= sprintf( '<div class="small-12 medium-%s large-%s columns"><a class="button fc-button" style="%s" href="%s">%s</a></div>', $cols, $cols, 'max-width:100%;', $button['link'], $button['text'] );
         }
 
         $content .= '<script type="text/javascript">' . file_get_contents(AG_CORE_DIR_PATH . 'js/flexiblecolumns_buttons.js') . '</script>';
@@ -78,76 +116,120 @@ function fc_repeating_content()
         // Get number of columns
         $count = (int)get_sub_field( 'count' );
 
-        // Cell content types are in the same repeater field
-        $headings = get_sub_field( 'headings' )[0];
-        $descriptions = get_sub_field( 'descriptions' )[0];
-        $links = get_sub_field( 'links' )[0];
-        $images = get_sub_field( 'images' )[0];
-
-        // Remove unused fields
-        if($images){
-          if($count == 2){
-            array_pop($images);
-            array_pop($images);
-            array_pop($images);
-          } else {
-            array_shift($images);
-            array_shift($images);
-          }
+        // Add the row title
+        $title = get_sub_field( 'columns_title' );
+        if ( $title ){
+          $content .= '<div class="small-12 medium-12 large-12 columns"><h2>' . $title . '</h2></div>';
         }
 
-        // Arrange values by cell
-        $row = array();
-        for( $i = 0; $i < $count; $i++ ){
-          $row[] = array(
-            'heading' => gettype($headings) == 'array' ? array_shift($headings) : '',
-            'columnimage' => gettype($images) == 'array' ? array_shift($images) : '',
-            'description' => gettype($descriptions) == 'array' ? array_shift($descriptions) : '',
-            'link' => gettype($links) == 'array' ? array_shift($links) : ''
-          );
+        // Get the type of content to render
+        $type = get_sub_field( 'type' );
+
+        if( $type == 'full' ){
+
+          // Full content in two columns
+          $leftcol = (int)get_sub_field('column_widths');
+          $content .= sprintf( '<div class="small-12 medium-%s large-%s columns">%s</div>', $leftcol, $leftcol, get_sub_field('column_one') );
+          $content .= sprintf( '<div class="small-12 medium-%s large-%s columns">%s</div>', 12 - $leftcol, 12 - $leftcol, get_sub_field('column_two') );
+
+        } else {
+
+          // Summarized content with headings, images, and/or descriptions
+          $headings = get_sub_field( 'headings' )[0];
+          $descriptions = get_sub_field( 'descriptions' )[0];
+          $links = get_sub_field( 'links' )[0];
+          $images = get_sub_field( 'images' )[0];
+
+          // Remove unused fields
+          if($images){
+            if($count == 2){
+              array_pop($images);
+              array_pop($images);
+              array_pop($images);
+            } else {
+              array_shift($images);
+              array_shift($images);
+            }
+          }
+
+          // Arrange values by cell
+          $row = array();
+          for( $i = 0; $i < $count; $i++ ){
+            $row[] = array(
+              'heading' => gettype($headings) == 'array' ? array_shift($headings) : '',
+              'columnimage' => gettype($images) == 'array' ? array_shift($images) : '',
+              'description' => gettype($descriptions) == 'array' ? array_shift($descriptions) : '',
+              'link' => gettype($links) == 'array' ? array_shift($links) : ''
+            );
+          }
+
+          // Determine the number of Foundation columns per cell
+          $cols = (12 - 12 % $count) / $count;
+
+          // Add each cell's content to output
+          foreach( $row as $cell ){
+
+            $heading = $cell['heading'];
+            $img = $cell['columnimage'];
+            $desc = $cell['description'];
+            $link = $cell['link'];
+            $linkopen = $linkclose = '';
+
+            if( $link != '' ){
+              $linkopen = '<a href="' . $link . '" style="display:block">';
+              $linkclose = '</a>';
+            }
+
+            if( $heading != '' ){
+              $heading = '<h3 class="summary-heading"><span>' . $heading . '</span></h3>';
+            }
+
+            if( $img != '' ){
+              $sizename = 'template-flexcolumns-' . $count;
+              $img = sprintf( '<img src="%s" alt="%s"/>', $img['sizes'][$sizename], $img['alt'] );
+              if( $link != '' )
+                $linkopen = str_replace('<a ', '<a class="flexible-columns-image-link" ', $linkopen);
+              if( $desc != '' )
+                $linkclose .= '<br>';
+            }
+
+            $content .= sprintf( '<div class="small-12 medium-%s large-%s columns summary">%s%s%s%s%s</div>', $cols, $cols, $linkopen, $heading, $img, $linkclose, $desc );
+
+          }
+
+          // Add script that sets equal height for column titls
+          $content .= '<script type="text/javascript">' . file_get_contents(AG_CORE_DIR_PATH . 'js/flexiblecolumns_headings.js') . '</script>';
+
         }
-
-        // Determine the number of Foundation columns per cell
-        $cols = (12 - 12 % $count) / $count;
-
-        // Add each cell's content to output
-        foreach( $row as $cell ){
-
-          $heading = $cell['heading'];
-          $img = $cell['columnimage'];
-          $desc = $cell['description'];
-          $link = $cell['link'];
-          $linkopen = $linkclose = '';
-
-          if( $link != '' ){
-            $linkopen = '<a href="' . $link . '" style="display:block">';
-            $linkclose = '</a>';
-          }
-
-          if( $heading != '' ){
-            $heading = '<h3><span>' . $heading . '</span></h3>';
-          }
-
-          if( $img != '' ){
-            $sizename = 'template-flexcolumns-' . $count;
-            $img = sprintf( '<img src="%s" alt="%s"/>', $img['sizes'][$sizename], $img['alt'] );
-            if( $link != '' )
-              $linkopen = str_replace('<a ', '<a class="flexible-columns-image-link" ', $linkopen);
-            if( $desc != '' )
-              $linkclose .= '<br>';
-          }
-
-          $content .= sprintf( '<div class="small-12 medium-%s large-%s columns">%s%s%s%s%s</div>', $cols, $cols, $linkopen, $heading, $img, $linkclose, $desc );
-
-        }
-
-        // Add script that sets equal height for column titls
-        $content .= '<script type="text/javascript">' . file_get_contents(AG_CORE_DIR_PATH . 'js/flexiblecolumns_headings.js') . '</script>';
 
       } else if( $rowname == 'accordion' ){
 
         $title = preg_replace( '/<(\/)?p>/', '<$1span>', get_sub_field( 'accordion_title' ) );
         $content .= sprintf( '<div class="small-12 medium-12 large-12 columns af-accordion"><a class="accordion-title" href="#" style="display: block;">%s</a><div class="accordion-content">%s</div></div>', $title, get_sub_field( 'accordion_content') );
+
+      } else if( $rowname == 'publications' ){
+
+        // Add top content
+        $title = get_sub_field( 'publications_title' );
+        $description = get_sub_field( 'publications_description' );
+
+        $content .= sprintf( '<div class="small-12 medium-12 large-12 columns"><h2>%s</h2><p>%s</p></div>',
+          $title, $description );
+
+        // Add publications
+        $content .= '<div class="small-12 medium-12 large-12 columns publications"><ul>';
+
+        while( has_sub_field('publications') ){
+
+          $title = get_sub_field( 'publication_title' );
+          $description = get_sub_field( 'publication_description' );
+          $overlay = get_sub_field( 'publication_overlay' );
+          $content .= sprintf( '<li><a class="open" href="javascript:;"><span class="title">%s</span><br /><span class="description">%s</span></a><div class="overlay"><a class="close" href="javascript:;" aria-label="Close"><span aria-hidden="true">×</span></a><div class="wrap"><div class="inner">%s</div><a class="close" href="javascript:;" aria-label="Close"><span aria-hidden="true">×</span></a></div></div></li>',
+            $title, $description, $overlay );
+
+        }
+
+        $content .= '</ul></div>';
 
       }
 
