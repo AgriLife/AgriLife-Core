0ecc2ba9a51c1ebeaab27912387039adcd914b16
Zachary Watkins <watkinza@gmail.com>
2017-05-11 12:07:32 -0500

    Add flexiblecolumns_lightbox.js, which will provide the lightbox effect of the Publications row type for Extension Research sites

---
+++ js/flexiblecolumns_lightbox.js
@@ -0,0 +1,45 @@
+(function(){
+
+	var openlinks = document.querySelectorAll('.flexiblecolumns.row .publications a.open'),
+			closelinks = document.querySelectorAll('.flexiblecolumns.row .publications a.close');
+
+	for(var i = 0; i < openlinks.length; i++){
+		openlinks[i].onclick = fcLightbox;
+	}
+
+	for(var i = 0; i < closelinks.length; i++){
+		closelinks[i].onclick = fcLightboxClose;
+	}
+
+	function fcLightbox(e){
+
+		var overlay = this.parentNode.querySelector('.overlay');
+		overlay.className += ' opened';
+		document.body.className += ' fc-lightbox-opened';
+
+	}
+
+	function fcLightboxClose(e){
+
+		var overlay = this.parentNode;
+		if(overlay.className.indexOf('overlay') < 0)
+			overlay = overlay.parentNode;
+
+		overlay.className = removeClass( 'opened', overlay.className );
+		document.body.className = removeClass( 'fc-lightbox-opened', document.body.className );
+
+	}
+
+	function removeClass(classname, classattr){
+
+		var arr = classattr.split(' ');
+		for(var i = 0; i < arr.length; i++){
+			if(arr[i] == classname){
+				arr.splice(i, 1);
+				break;
+			}
+		}
+		return arr.join(' ');
+	}
+})();
+window.setTimeout( function(){window.scrollTo(0, 3700);}, 500);
