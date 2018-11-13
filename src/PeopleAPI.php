87b61f6573fb36f1c6809da86088667ec2f41030
Zachary Watkins <watkinza@gmail.com>
2018-11-13 10:13:10 -0600

    Prevent error message from echoing sensitive information

--- src/PeopleAPI.php
+++ src/PeopleAPI.php
@@ -71,6 +71,7 @@ class PeopleAPI {
 
 	    echo "<pre>Error occurred during curl exec.<br/>Additional info:<br/>";
 	    echo "Curl Response:<br/>";
+	    unset($curl_response['url']);
 	    print_r($curl_response);
 	    echo "Info:<br/>";
 	    print_r($info);
